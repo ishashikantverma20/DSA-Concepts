01b3bb8bfec3b049f5f4afbcd682f534dd5ced22
Shashikant Verma <ishashikantverma20@gmail.com>
2023-11-24 23:52:19 +0530

    Added solution for problems  - PascalTriangle  - ReverseLinkedList  - SearchInBST

--- Recursion/src/main/java/SearchInBST.java
+++ Recursion/src/main/java/SearchInBST.java
@@ -2,9 +2,9 @@
  * You are given the root of a binary search tree (BST) and an integer val.
  *
  * Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
- *                   6
- *              4       10
- *           2   5    7    12
+ *                   4
+ *              2       7
+ *           1   3
  *
  *     Input: root = [4,2,7,1,3], val = 2
  *     Output: [2,1,3]
